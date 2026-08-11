e90773f48e02f5e5d1d4a84eba38cfe529c63511
Pedro Simoes <pedrosimoes1298@gmail.com>
2026-08-11 09:34:53 -0300

    Projeto iniciado + integração com o Leaflet (mapa)

---
+++ script.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([-14.8612, -40.8444], 16);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
